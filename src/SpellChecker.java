7cdbab5bed841eb63975d1c1bdc66832f8a2d439
ScottyOBrien <obrie023@rangers.uwp.edu>
2019-03-19 17:27:33 -0500

    added Lexicon class and several helper methods.

--- src/SpellChecker.java
+++ src/SpellChecker.java
@@ -1,4 +1,64 @@
+import java.io.*;
+import java.util.*;
+
+/**
+ * @author Scott O'Brien
+ * @version 3/12/2019
+ * This project uses a data structure known as a Trie. It will implement a spell checker and an inner class named
+ * Lexicon that is implemented as a Trie.
+ */
 public class SpellChecker {
+
+    static final int SIZE = 26;
+
+    public static void main(String[] args) {
+        List<String> stringList = new ArrayList<>();
+    }
+
+    class Lexicon {
+        /**
+         * Lexicon has constructor and one public method, and 1 or more private helper methods. Will read all words from
+         * enable1augmented.txt and build a Trie containing them.
+         * containsWord (String word) is a public boolean method which returns true if the word is in the lexicon, false
+         * otherwise.
+         * @param LexNode This param contains our root of our Trie.
+         */
+
+        final LexNode root;
+
+        public Lexicon (){
+            this.root = new LexNode();
+        }
+
+        public String search(final String word){
+            LexNode current = root;
+            for (int i = 0; i < word.length(); i++) {
+                if (current==null || current.next(word.charAt(i))==null){
+                    return "string does not exist!";
+                }
+            }
+            return word + " exists!";
+        }
+
+        public void insert(final String word){
+            LexNode current = root;
+            for (int i = 0; i < word.length(); i++) {
+                if (current.lexNodes[word.charAt(i) - 'a'] == null) {
+                    current.lexNodes[word.charAt(i) - 'a'] = new LexNode();
+                }
+            }
+        }
+
+    }
+
+    class LexNode {
+        final LexNode[] lexNodes = new LexNode[SIZE];
+
+        public LexNode next(final char c) {
+            return lexNodes[c-'a'];
+        }
+    }
+
 }
 
 
